29ecedd4b6ba2c1013bba48aa42381207f16fe71
挨踢方丈 <itabbot@qq.com>
2022-12-05 20:42:50 +0800

    主干逻辑。

--- src/service.js
+++ src/service.js
@@ -1,12 +1,15 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-await-in-loop */
+
 const config = require('config');
 const uuid = require('uuid');
 const { sleep, error500 } = require('./libraries/utils');
 const { getRedis } = require('./libraries/redis');
+const { handleError } = require('./libraries/error-handling');
 const dao = require('./dao');
+const formulas = require('./libraries/formulas');
 const logger = require('./libraries/logger')('service', {
   level: 'info',
-  noErrorFile: true,
-  consoleNot: true,
 });
 
 // 消费者名称
@@ -74,38 +77,123 @@ const readMsgs = async () => {
   return taskIds;
 };
 
-//
-//
-//
-//
-//
-//
-//
-//
-// <<<<<
-
 /**
  * 查询任务
  * @param {array} taskIds 任务ID列表
  * @returns {array}
  */
 const queryTasks = async (taskIds) => {
-  //
-  //
-  //
-  //
-  //
-  //
-
   // 查询任务
-  const tasks = await dao.queryTasks(taskIds);
+  const rsts = await dao.queryTasks(taskIds);
 
   // 任务检查
+  const tasks = [];
+  for (let i = 0; i < taskIds.length; i += 1) {
+    const taskId = taskIds[i];
+    const task = rsts.find((t) => t.id === taskId);
+
+    if (!task) {
+      logger.error(`任务不存在（taskId=${taskId}）`);
+      continue;
+    }
+
+    if (!task.status !== 'pending') {
+      logger.error(`任务不是 pending 状态（taskId=${taskId}）`);
+      continue;
+    }
+
+    if (!task.autoCompensate !== 'enabled') {
+      logger.error(`任务不是不允许自动理赔（taskId=${taskId}）`);
+      continue;
+    }
+
+    tasks.push(task);
+  }
+  if (tasks.length <= 0) return [];
 
   // 更新状态为处理中
   await dao.handingTasks(tasks);
 
   return tasks;
+};
+
+/**
+ * 执行赔付
+ * @param {object} task 赔付任务
+ */
+const compensation = async (task) => {
+  const { Claim: claim } = task;
+  const { premium, autoCompensate } = claim.bizConfigParsed;
+  const { calculateMode, formula, fixed } = premium;
+  const { maximum } = autoCompensate;
+
+  // 写入开始处理时间
+  await dao.updateCompensationTask({ handledAt: Date.now() }, { id: task.id });
+
+  // 计算保额
+  if (calculateMode === 'fixed') {
+    claim.sumInsured = 0;
+    claim.insureds.forEach((insured, i) => {
+      claim.insureds[i].sumInsured = fixed;
+      claim.sumInsured += fixed;
+    });
+  } else if (calculateMode === 'formula') {
+    const { name, params } = formula;
+
+    if (
+      !hasOwnProperty(formulas, name) ||
+      typeof formulas[name] !== 'function'
+    ) {
+      throw error500('计费公式有误');
+    }
+
+    const ctx = {
+      claim,
+      policy: claim.Policy,
+    };
+    formulas[name](ctx, 'claim', params);
+    claim.sumInsured = ctx.claim.sumInsured;
+    claim.insureds = ctx.claim.insureds;
+  }
+
+  // 保额校验
+  let totalSumInsured = 0;
+  claim.insureds.forEach((insured) => {
+    totalSumInsured += insured.sumInsured;
+  });
+  if (totalSumInsured !== claim.sumInsured) {
+    throw error500(`计费错误，被保险人总保额不等于理赔单总保额`);
+  }
+  if (claim.sumInsured > maximum) {
+    throw error500('赔付金额大于允许的范围');
+  }
+
+  // 理赔成功，更新相关数据
+  await dao.compensationSuccessed({ claim, task });
+};
+
+/**
+ * 执行处理
+ * @param {object} task 赔付任务
+ */
+const handler = async (task) => {
+  try {
+    await compensation(task);
+  } catch (error) {
+    // 处理失败记录原因
+    await dao.updateCompensationTask(
+      {
+        status: 'failure',
+        finishedAt: Date.now(),
+        failureReasons: JSON.stringify({
+          message: error.message,
+          stack: error.stack,
+          ...error,
+        }),
+      },
+      { id: task.id },
+    );
+  }
 };
 
 /**
@@ -118,126 +206,23 @@ const start = async () => {
   // 轮询
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    // 停顿
-    // eslint-disable-next-line no-await-in-loop
-    await sleep(2000);
+    try {
+      // 停顿
+      await sleep(2000);
 
-    // 读取队列消息
-    // eslint-disable-next-line no-await-in-loop
-    const taskIds = await readMsgs();
+      // 读取队列消息
+      const taskIds = await readMsgs();
 
-    //
-    //
-    //
-    //
-    //
-    //
+      // 查询任务数据
+      const tasks = await queryTasks(taskIds);
 
-    // 查询任务数据
-    // const tasks =
-    // eslint-disable-next-line no-await-in-loop
-    await queryTasks(taskIds);
-
-    // 执行处理
+      // 执行处理
+      await Promise.all(tasks.map((task) => handler(task)));
+    } catch (error) {
+      handleError(error);
+    }
   }
 };
-
-// module.exports = async () => {
-//   // 查询待处理的任务
-//   const tasks = await queryPendingTasks();
-
-//   // 拆分批次
-//   const trunks = _.chunk(tasks, 10);
-
-//   // 分批执行处理
-//   for (let i = 0; i < trunks.length; i += 1) {
-//     // eslint-disable-next-line no-await-in-loop
-//     await Promise.all(trunks[i].map((task) => handler(task)));
-//   }
-// };
-
-// const _ = require('lodash');
-// const dao = require('./dao');
-// const formulas = require('../../libraries/formulas');
-
-// /**
-//  * 执行赔付
-//  * @param {object} task 赔付任务
-//  */
-// const compensation = async (task) => {
-//   const { Claim: claim } = task;
-//   const { premium, autoCompensate } = claim.bizConfigParsed;
-//   const { calculateMode, formula, fixed } = premium;
-//   const { maximum } = autoCompensate;
-
-//   // 写入开始处理时间
-//   await dao.updateCompensationTask({ handledAt: Date.now() }, { id: task.id });
-
-//   // 计算保额
-//   if (calculateMode === 'fixed') {
-//     claim.sumInsured = 0;
-//     claim.insureds.forEach((insured, i) => {
-//       claim.insureds[i].sumInsured = fixed;
-//       claim.sumInsured += fixed;
-//     });
-//   } else if (calculateMode === 'formula') {
-//     const { name, params } = formula;
-
-//     if (
-//       !hasOwnProperty(formulas, name) ||
-//       typeof formulas[name] !== 'function'
-//     ) {
-//       throw error500('计费公式有误');
-//     }
-
-//     const ctx = {
-//       claim,
-//       policy: claim.Policy,
-//     };
-//     formulas[name](ctx, 'claim', params);
-//     claim.sumInsured = ctx.claim.sumInsured;
-//     claim.insureds = ctx.claim.insureds;
-//   }
-
-//   // 保额校验
-//   let totalSumInsured = 0;
-//   claim.insureds.forEach((insured) => {
-//     totalSumInsured += insured.sumInsured;
-//   });
-//   if (totalSumInsured !== claim.sumInsured) {
-//     throw error500(`计费错误，被保险人总保额不等于理赔单总保额`);
-//   }
-//   if (claim.sumInsured > maximum) {
-//     throw error500('赔付金额大于允许的范围');
-//   }
-
-//   // 理赔成功，更新相关数据
-//   await dao.compensationSuccessed({ claim, task });
-// };
-
-// /**
-//  * 执行处理
-//  * @param {object} task 赔付任务
-//  */
-// const handler = async (task) => {
-//   try {
-//     await compensation(task);
-//   } catch (error) {
-//     // 处理失败记录原因
-//     await dao.updateCompensationTask(
-//       {
-//         status: 'failure',
-//         finishedAt: Date.now(),
-//         failureReasons: JSON.stringify({
-//           message: error.message,
-//           stack: error.stack,
-//           ...error,
-//         }),
-//       },
-//       { id: task.id },
-//     );
-//   }
-// };
 
 module.exports = {
   start,
